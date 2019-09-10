1a797666bbfeec47443839863b5da69f36557130
Gabriel Coronado <gabrielomar2809@gmail.com>
2019-09-10 10:50:30 -0500

    Add action cable spec require capybara/apparition in rails_helper

---
+++ spec/features/send_messages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature 'Send messages' do
+  scenario 'properly' do
+    user = create(:user)
+    signin_as user
+    msg = create(:message, user: user)
+    fill_in 'message_body', with: msg.body
+    expect { click_button('button') }
+            .to change(Message, :count).by(1)
+    expect(page).to have_text(msg.user.username)
+    expect(page).to have_text(msg.body)
+  end
+end
